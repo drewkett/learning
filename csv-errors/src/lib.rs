692d1dd9a730baa5f8214a3e3540c77bff9e53ef
Eli Flanagan <efx@users.noreply.github.com>
2020-05-26 13:33:56 -0400

    error deserialization

--- csv-errors/src/lib.rs
+++ csv-errors/src/lib.rs
@@ -1,7 +1,36 @@
+use serde::Deserialize;
+#[derive(Deserialize)]
+pub enum Reservation {
+    /// yes I am attending
+    Y,
+    /// no I cannot make it
+    N,
+    /// noncommital responses
+    M,
+}
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use csv::ErrorKind;
+    use csv::Reader;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn reservation_invalid() {
+        let reservations = "reservation
+        z";
+
+        let mut reader = Reader::from_reader(reservations.as_bytes());
+        for result in reader.deserialize() {
+            match result {
+                Ok(raw) => {
+                    let _record: Reservation = raw;
+                }
+                Err(err) => match err.kind() {
+                    ErrorKind::Deserialize { err: other, pos: _ } => {
+                        assert_eq!(other.to_string(), "invalid Reservation code provided");
+                    }
+                    _ => {}
+                },
+            };
+        }
     }
 }
