56a24c9ebc263dd4d7f7069f54d888b7d721fe14
Eli Flanagan <efx@users.noreply.github.com>
2020-05-28 07:19:38 -0400

    simple deserialize example

--- csv-errors/src/lib.rs
+++ csv-errors/src/lib.rs
@@ -1,12 +1,21 @@
+use csv::Reader;
 use serde::Deserialize;
+use std::io::Write;
+
 #[derive(Deserialize)]
-pub enum Reservation {
-    /// yes I am attending
-    Y,
-    /// no I cannot make it
-    N,
-    /// noncommital responses
-    M,
+pub struct Reservation {
+    guest: String,
+    checked_in: bool,
+}
+/// count the number of reservations
+pub fn count_reservations(bytes: &[u8]) -> Result<usize, Box<dyn std::error::Error>> {
+    let mut reader = Reader::from_reader(bytes);
+    let mut count = 0;
+    for result in reader.deserialize() {
+        let _record: Reservation = result?;
+        count += 1;
+    }
+    Ok(count)
 }
 #[cfg(test)]
 mod tests {
@@ -14,23 +23,10 @@ mod tests {
     use csv::ErrorKind;
     use csv::Reader;
     #[test]
-    fn reservation_invalid() {
-        let reservations = "reservation
-        z";
-
-        let mut reader = Reader::from_reader(reservations.as_bytes());
-        for result in reader.deserialize() {
-            match result {
-                Ok(raw) => {
-                    let _record: Reservation = raw;
-                }
-                Err(err) => match err.kind() {
-                    ErrorKind::Deserialize { err: other, pos: _ } => {
-                        assert_eq!(other.to_string(), "invalid Reservation code provided");
-                    }
-                    _ => {}
-                },
-            };
-        }
+    fn reservation_count() {
+        let reservations = "guest,checked_in
+        fred,false";
+        let count = count_reservations(reservations.as_bytes()).unwrap();
+        assert_eq!(count, 1);
     }
 }
